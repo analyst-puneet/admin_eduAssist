8b232eccd2c93a954de09c7a8a15f801fa29993e
Hemant kumar <122089115+Hemant4556@users.noreply.github.com>
2025-04-18 11:25:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import { AuthProvider } from "app/contexts/AuthContext";
 import App from "./app/App";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 let BASE_URL = "https://backend-aufx.onrender.com/";
-if (window.location.host == "localhost") {
-  BASE_URL = "http://localhost:5000";
-}
 export { BASE_URL };
 const root = createRoot(document.getElementById("root"));
 
