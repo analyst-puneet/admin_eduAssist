a1ca2a0cbe44f4c8936832bb44fe0c37c6a4b216
Hemant kumar <122089115+Hemant4556@users.noreply.github.com>
2025-04-18 11:39:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "app/contexts/AuthContext";
 import App from "./app/App";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
-let BASE_URL = "https://backend-aufx.onrender.com/";
+let BASE_URL = "https://backend-aufx.onrender.com";
 export { BASE_URL };
 const root = createRoot(document.getElementById("root"));
 
